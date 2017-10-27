00d923511f358122be16f6a9bf0fd8f204e6c673
rhee <you@example.com>
2017-10-27 05:40:40 +0200

    b2

---
+++ temp/cache/Nette.RobotLoader/8843efbb2aa7de6fbcd9d24071165b24.php
@@ -0,0 +1,62 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Forms\\FormFactory' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\forms\\FormFactory.php',
+      'time' => 1504882448,
+    ),
+    'App\\Forms\\SignInFormFactory' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\forms\\SignInFormFactory.php',
+      'time' => 1504882448,
+    ),
+    'App\\Forms\\SignUpFormFactory' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\forms\\SignUpFormFactory.php',
+      'time' => 1504882448,
+    ),
+    'App\\Model\\UserManager' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\model\\UserManager.php',
+      'time' => 1504882448,
+    ),
+    'App\\Model\\DuplicateNameException' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\model\\UserManager.php',
+      'time' => 1504882448,
+    ),
+    'App\\Presenters\\BasePresenter' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\presenters\\BasePresenter.php',
+      'time' => 1504882448,
+    ),
+    'App\\Presenters\\Error4xxPresenter' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\presenters\\Error4xxPresenter.php',
+      'time' => 1504882448,
+    ),
+    'App\\Presenters\\ErrorPresenter' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\presenters\\ErrorPresenter.php',
+      'time' => 1504882448,
+    ),
+    'App\\Presenters\\HomepagePresenter' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\presenters\\HomepagePresenter.php',
+      'time' => 1504882448,
+    ),
+    'App\\Presenters\\SignPresenter' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\presenters\\SignPresenter.php',
+      'time' => 1504882448,
+    ),
+    'App\\RouterFactory' => 
+    array (
+      'file' => 'C:\\Program Files (x86)\\Ampps\\www\\tes\\app\\router\\RouterFactory.php',
+      'time' => 1504882448,
+    ),
+  ),
+  1 => NULL,
+);
